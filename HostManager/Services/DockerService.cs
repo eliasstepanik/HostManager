e126ae7dcebcb13be4315223df0750d8f7bd0596
VWI7M0N <elias.stepanik@volkswagen.de>
2023-11-03 21:22:06 +0100

    d

--- HostManager/Services/DockerService.cs
+++ HostManager/Services/DockerService.cs
@@ -1,6 +1,9 @@
 ﻿using Docker.DotNet;
 using Docker.DotNet.Models;
+using Google.Protobuf.Collections;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
+using Microsoft.Extensions.Logging;
 
 namespace HostManager.Services;
 
@@ -16,32 +19,43 @@ public class DockerService : HostManager.DockerService.DockerServiceBase
 
     }
     
-    public async Task<GetPortsRequest?> GetPorts()
+    public override async Task<GetPortsReply> GetPorts(GetPortsRequest request, ServerCallContext context)
     {
         var containers = await _client.Containers.ListContainersAsync(new ContainersListParameters());
 
-        var portsRequest = new GetPortsRequest()
-        {
-            Ports = new List<Port>()
-        };
+        var ports = new List<GetPortsReply.Types.Port>();
         foreach (var container in containers)
         {
             foreach (var containerPort in container.Ports)
             {
-                if (containerPort.PublicPort != 0 && containerPort.PublicPort != 443 && containerPort.PublicPort != 80)
+                if (containerPort.PublicPort != 0)
                 {
-                    Port port = new Port()
+                    GetPortsReply.Types.Port.Types.ProtocolType proto;
+                    if (containerPort.Type.Equals("tcp"))
+                        proto = GetPortsReply.Types.Port.Types.ProtocolType.Tcp;
+                    else
+                        proto = GetPortsReply.Types.Port.Types.ProtocolType.Udp;
+                    
+                    var port = new GetPortsReply.Types.Port()
                     {
-                        Proto = containerPort.Type,
-                        Dport = containerPort.PublicPort
+                        Action = GetPortsReply.Types.Port.Types.ActionType.Accept,
+                        Enable = true,
+                        Position = 0,
+                        Protocol = proto,
+                        Type = GetPortsReply.Types.Port.Types.TypeType.In,
+                        DestPort = containerPort.PublicPort
+                        
                     };
-                    if(!portsRequest.Ports.Contains(port))
-                        portsRequest.Ports.Add(port);
+                    ports.Add(port);
                 }
                 
             }
         }
-        
-        return portsRequest;
+        return await Task.FromResult(new GetPortsReply
+        {
+            Ports = { ports },
+            Timestamp = DateTime.Now.ToTimestamp()
+        });
     }
+    
 }
